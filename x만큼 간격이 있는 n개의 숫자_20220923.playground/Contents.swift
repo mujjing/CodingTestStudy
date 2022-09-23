15508f298287ff9d3292f37df7fc9db785efe814
j-jihoon <junji636@gmail.com>
2022-09-23 21:47:21 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ x만큼 간격이 있는 n개의 숫자_20220923.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+
+//x    n    answer
+//2    5    [2,4,6,8,10]
+//4    3    [4,8,12]
+//-4    2    [-4, -8]
+
+var result: [Int64] = []
+var num = 0
+
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    
+    for i in 1...n {
+        num = x * i
+        result.append(Int64(num))
+    }
+    
+    return result
+}
+
+solution(-4, 2)
