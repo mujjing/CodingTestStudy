15acb328b2b01529b17d58a4157ee59488bc4392
j-jihoon <junji636@gmail.com>
2022-09-24 10:25:31 +0900

    나머지가 1이 되는 수 찾기

---
+++ 나머지가 1이 되는 수 찾기_20220924.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+import Foundation
+
+//자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+
+
+//n    result
+//10    3
+//12    11
+
+var result = true
+var resultNum = 1
+var num = 0
+
+func solution(_ n:Int) -> Int {
+    num = n
+    
+    while result {
+        if num % resultNum == 1 {
+            break
+        } else {
+            resultNum += 1
+        }
+    }
+    
+    return resultNum
+}
+
+solution(10)
